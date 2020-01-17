a35f77b1b47339b701dda6127f3e1bfbec968bd4
sshaiakhmedov <e.shayakhmedov@gmail.com>
2020-01-17 01:55:49 -0700

    User Login Unit Testing

--- test/specs/register.js
+++ test/specs/register.js
@@ -3,84 +3,118 @@ const email=Math.random()+'e.shayakhmedov@gmail.com';
 const firstName='John';
 const lastName='Shaiakhmedov';
 
+// describe('Register page', () => {
+//     it('should have the right title', () => {
+//         browser.url('https://stage.pasv.us/user/register/')
+//         const actualTitle = browser.getTitle();
+//         const expectedTitle = 'Progress Monitor';
+//         expect(actualTitle).eq(expectedTitle)
+//     })
+//     it ('should have a correct title', () => {
+//         const actualH1Text = $('h1').getText ();
+//         const expectedH1Text = 'User Register';
+//         expect (actualH1Text).equal (expectedH1Text);
+//     });
+//
+//     it ('should have a correct description', () => {
+//         const actual = $('p').getText ();
+//         const expected= 'Profiles with fictitious or dummy data will be deleted.';
+//         expect (actual).equal (expected);
+//     });
+//
+//     it ('should have a correct button text', () => {
+//         const actual = $('form button').getText ();
+//         const expected= 'Submit';
+//         expect (actual).equal (expected);
+//     });
+//
+//     it ('should fill First Name field', () => {
+//         const element=$('form input[name="firstName"]')
+//         element.setValue(firstName);
+//         browser.pause(2000);
+//     });
+//
+//     it ('should fill Last Name field', () => {
+//         const element=$('form input[name="lastName"]')
+//         element.setValue(lastName);
+//         browser.pause(2000);
+//     });
+//
+//     it ('should fill up phone number', () => {
+//         const element=$('form input[name="phone"]')
+//         element.setValue('12345678900');
+//     });
+//
+//     it ('should fill up e-mail address', () => {
+//         const element=$('form input[name="email"]')
+//         element.setValue(email);
+//     });
+//
+//     it ('should fill up password', () => {
+//         const element=$('form input[name="password"]')
+//         element.setValue('3473512');
+//     });
+//
+//     it ('should fill About field', () => {
+//         const element=$('form textarea[name="about"]')
+//         element.setValue('123');
+//     });
+//
+//     it ('should fill Goals field', () => {
+//         const element=$('form textarea[name="goals"]')
+//         element.setValue('paradise');
+//     });
+//
+//     it ('should fill English level from drop-down list', () => {
+//         const element=$('form select[name="englishLevel"]')
+//         element.selectByVisibleText('Upper intermediate');
+//     });
+//
+//     it ('should click the Button', () => {
+//         const element=$('form button[type="submit"]');
+//         element.click();
+//         browser.pause(2000);
+//     });
+//
+//     /*login title
+//     email pass
+//     login button
+//     wait
+//     title firstName lastName
+//      */
+// });
 
-describe('Register page', () => {
+describe('User Login should let the registered user login', () => {
+
     it('should have the right title', () => {
-        browser.url('https://stage.pasv.us/user/register/')
+        browser.url('https://stage.pasv.us/user/login/')
         const actualTitle = browser.getTitle();
         const expectedTitle = 'Progress Monitor';
-        expect(actualTitle).eq (expectedTitle)
+        expect(actualTitle).eq(expectedTitle)
     })
-    it ('should have a correct title', () => {
+
+    it ('should have a correct h1 title', () => {
         const actualH1Text = $('h1').getText ();
-        const expectedH1Text = 'User Register';
-        expect (actualH1Text).equal (expectedH1Text);
-    });
-
-    it ('should have a correct description', () => {
-        const actual = $('p').getText ();
-        const expected= 'Profiles with fictitious or dummy data will be deleted.';
-        expect (actual).equal (expected);
-    });
-
-    it ('should have a correct button text', () => {
-        const actual = $('form button').getText ();
-        const expected= 'Submit';
-        expect (actual).equal (expected);
-    });
-
-    it ('should fill First Name field', () => {
-        const element=$('form input[name="firstName"]')
-        element.setValue(firstName);
+        const expectedH1Text = 'User Login';
+        expect(actualH1Text).eq(expectedH1Text);
         browser.pause(2000);
-    });
-
-    it ('should fill Last Name field', () => {
-        const element=$('form input[name="lastName"]')
-        element.setValue(lastName);
-        browser.pause(2000);
-    });
-
-    it ('should fill up phone number', () => {
-        const element=$('form input[name="phone"]')
-        element.setValue('12345678900');
-    });
+    })
 
     it ('should fill up e-mail address', () => {
-        const element=$('form input[name="email"]')
-        element.setValue(email);
-    });
+       const element=$('form input[name="email"]')
+        element.setValue('e.shayakhmedov@gmail.com');
+        browser.pause(2000);
+    })
 
     it ('should fill up password', () => {
-        const element=$('form input[name="password"]')
-        element.setValue('3473512');
-    });
-
-    it ('should fill About field', () => {
-        const element=$('form textarea[name="about"]')
-        element.setValue('123');
-    });
-
-    it ('should fill Goals field', () => {
-        const element=$('form textarea[name="goals"]')
-        element.setValue('paradise');
-    });
-
-    it ('should fill English level from drop-down list', () => {
-        const element=$('form select[name="englishLevel"]')
-        element.selectByVisibleText('Upper intermediate');
-    });
-
-    it ('should click the Button', () => {
-        const element=$('form button[type="submit"]');
-        element.click();
+         const element=$('form input[name="password"]')
+         element.setValue('3473512');
         browser.pause(2000);
-    });
+     })
 
-    /*login title
-    email pass
-    login button
-    wait
-    title firstName lastName
-     */
-})
+     it ('should click the Login button', () => {
+         const element = $('form button[type="submit"]');
+         element.click();
+         browser.pause(2000);
+     })
+});
