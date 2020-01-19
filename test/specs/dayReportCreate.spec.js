87a910dce438bdbe2576dd0476c4354c2979e482
sshaiakhmedov <e.shayakhmedov@gmail.com>
2020-01-18 20:14:18 -0700

    Data file and Tests are separate

--- test/specs/dayReportCreate.spec.js
+++ test/specs/dayReportCreate.spec.js
@@ -1,27 +1,13 @@
-const {expect} = require ('chai');
-const email=Math.random().toFixed(4)+'e.shayakhmedov@gmail.com';
-
-const url_log='https://stage.pasv.us/user/login/';
-const user ={
-    firstName: 'John',
-    lastName: 'Shaiakhmedov',
-    password: 'password',
-    phone: '12345678900',
-    email: email,
-    about: '123',
-    goals: 'paradise',
-    englishLevel: 'Upper intermediate',
-};
-
+const {expect} = require('chai');
+const {urlLogin} = require('./register_data');
 
 describe('Create Day report', () => {
     it('should Login as admin', () => {
-        browser.url(url_log);
+        browser.url(urlLogin);
         $('form input[name="email"]').setValue('olya6avg@gmail.com');
         $('form input[name="password"]').setValue('11111');
         $('form button[type="submit"]').click();
-        browser.pause(5000);
-
+        browser.pause(4000);
     });
 });
 
--- test/specs/register.spec.js
+++ test/specs/register.spec.js
@@ -1,96 +1,72 @@
+
+const {urlRegister, urlLogin, user, pageReg, pageRegSelector, pageLog}=require('./register_data');
 const {expect} = require ('chai');
-const email=Math.random().toFixed(4)+'e.shayakhmedov@gmail.com';
-
-const url_reg='https://stage.pasv.us/user/register';
-const url_log='https://stage.pasv.us/user/login/';
-const user ={
-    firstName: 'John',
-    lastName: 'Shaiakhmedov',
-    password: 'password',
-    phone: '12345678900',
-    email: email,
-    about: '123',
-    goals: 'paradise',
-    englishLevel: 'Upper intermediate',
-};
-
-const page_reg={
-    title: 'Progress Monitor',
-    h1: 'User Register',
-    description: 'Profiles with fictitious or dummy data will be deleted.',
-    buttonText: 'Submit',
-};
-
-const page_log={
-    title: 'Progress Monitor',
-    h1: 'User Login',
-};
 
 describe('Register page', () => {
     it('should have the right title', () => {
-        browser.url(url_reg);
+        browser.url(urlRegister);
         const actualTitle = browser.getTitle();
-        const expectedTitle = page_reg.title;
+        const expectedTitle = pageReg.title;
         expect(actualTitle).eq(expectedTitle)
     });
 
     it ('should have a correct title', () => {
-        const actualH1Text = $('h1').getText ();
-        const expectedH1Text = page_reg.h1;
-        expect (actualH1Text).equal (expectedH1Text);
+        const actualH1Text = $(pageRegSelector.h1).getText ();
+        const expectedH1Text = pageReg.h1;
+        expect(actualH1Text).equal(expectedH1Text);
     });
 
     it ('should have a correct description', () => {
-        const actual = $('p').getText ();
-        const expected= page_reg.description;
-        expect (actual).equal (expected);
+        const actual = $(pageRegSelector.descr).getText ();
+        const expected= pageReg.description;
+        expect(actual).equal(expected);
     });
 
     it ('should have a correct button text', () => {
-        const actual = $('form button').getText ();
-        const expected= page_reg.buttonText;
-        expect (actual).equal (expected);
+        const actual = $(pageRegSelector.submitB).getText ();
+        const expected= pageReg.buttonText;
+        expect(actual).equal(expected);
     });
 
     it ('should fill First Name field', () => {
-        const element=$('form input[name="firstName"]');
+        const element=$(pageRegSelector.firstNameInput);
         element.setValue(user.firstName);
         browser.pause(2000);
     });
 
     it ('should fill Last Name field', () => {
-        const element=$('form input[name="lastName"]');
+        const element=$(pageRegSelector.lastNameInput);
         element.setValue(user.lastName);
         browser.pause(2000);
     });
 
     it ('should fill up phone number', () => {
-        const element=$('form input[name="phone"]');
+        const element=$(pageRegSelector.phoneInput);
         element.setValue(user.phone);
     });
 
     it ('should fill up e-mail address', () => {
-        const element=$('form input[name="email"]');
+        const element=$(pageRegSelector.emailInput);
         element.setValue(user.email);
     });
 
     it ('should fill up password', () => {
-        const element=$('form input[name="password"]');
+        const element=$(pageRegSelector.passwordInput);
         element.setValue(user.password);
     });
 
     it ('should fill About field', () => {
-        const element=$('form textarea[name="about"]');
+        const element=$(pageRegSelector.aboutInput);
         element.setValue(user.about);
     });
 
     it ('should fill Goals field', () => {
-        const element=$('form textarea[name="goals"]');
+        const element=$(pageRegSelector.goalInput);
         element.setValue(user.goals);
     });
 
     it ('should fill English level from drop-down list', () => {
-        const element=$('form select[name="englishLevel"]');
+        const element=$(pageRegSelector.englishInput);
         element.selectByVisibleText(user.englishLevel);
     });
 
@@ -103,17 +79,16 @@ describe('Register page', () => {
 });
 
 describe('User Login should let the registered user login', () => {
-
     it('should have the right title', () => {
-        browser.url(url_log);
+        browser.url(urlLogin);
         const actualTitle = browser.getTitle();
-        const expectedTitle = page_log.title;
+        const expectedTitle = pageLog.title;
         expect(actualTitle).eq(expectedTitle)
     });
 
     it ('should have a correct h1 title', () => {
-        const actualH1Text = $('h1').getText ();
-        const expectedH1Text = page_log.h1;
+        const actualH1Text = $('h1').getText();
+        const expectedH1Text = pageLog.h1;
         expect(actualH1Text).eq(expectedH1Text);
         browser.pause(2000);
     });
@@ -121,27 +96,16 @@ describe('User Login should let the registered user login', () => {
     it ('should fill up e-mail address', () => {
        const element=$('form input[name="email"]');
         element.setValue(user.email);
-        browser.pause(2000);
     });
 
     it ('should fill up password', () => {
          const element=$('form input[name="password"]');
          element.setValue(user.password);
-        browser.pause(2000);
      });
 
      it ('should click the Login button', () => {
          const element = $('form button[type="submit"]');
          element.click();
-         browser.pause(3000);
+         browser.pause(5000);
      })
 });
-
-// describe('New User Confirmation page', () => {
-//
-//     it('should have the right title', () => {
-//         browser.url(url_log);
-//         const actualTitle = browser.getTitle();
-//         const expectedTitle = page_log.title;
-//         expect(actualTitle).eq(expectedTitle)
-//     });
