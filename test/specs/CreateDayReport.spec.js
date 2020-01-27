7443c335950f889cb30b939ea01549e20b02189f
sshaiakhmedov <e.shayakhmedov@gmail.com>
2020-01-26 21:14:48 -0700

    Users, Create daily report

---
+++ test/specs/CreateDayReport.spec.js
@@ -0,0 +1,69 @@
+const {expect} = require('chai');
+const {admin, urlDiary, urlLogin} = require('./register_data');
+
+describe('Create daily report', () => {
+    it('should Login as admin', () => {
+        browser.url(urlLogin);
+        $('form input[name="email"]').setValue(admin.emailLogin);
+        $('form input[name="password"]').setValue(admin.password);
+        $('form button[type="submit"]').click();
+        browser.pause(2000);
+    });
+
+    it('should go to Users page', () => {
+        const element = $('#site-menu > ul > li:nth-child(6) > a');
+        //const element = $('//a[@class='nav-link active']');
+        element.click();
+    });
+
+    it('should have Daily reports h1 title', () => {
+        const actualH1Title = $('//h1[contains(text(),\'Daily reports\')]').getText();
+        const expectedH1Title = 'Daily reports';
+        expect(actualH1Title).eq(expectedH1Title);
+        browser.pause(1000);
+    });
+
+    it('should click Create day report button', () => {
+        const element = $('//a[@class=\'btn btn-secondary\']');
+        element.click();
+        browser.pause(1000);
+    });
+
+    it('should have Create day report h1', () => {
+        const actualH1title = $('h1').getText();
+        const expectedH1title='Create day report';
+        expect(actualH1title).eq(expectedH1title);
+        browser.pause(1000);
+    });
+
+    it('should mark Code Practis', () => {
+        const element = $('//input[@id=\'input-5\']');
+        element.click();
+    });
+
+    it('should select 7 from What is your Morale range', () => {
+        const element = $('//select[contains(@name,\'morale\')]');
+        element.selectByVisibleText('7');
+        browser.pause(1000);
+    });
+
+    it('should put 5 to How many hours did you study/practice today? field', () => {
+        const element = $('//input[contains(@name,\'hours\')]');
+        element.setValue('5')
+        browser.pause(1000);
+    });
+
+    it('should enter >=30 characters to the text area How was your day?', () => {
+        const element = $('//textarea[contains(@name,\'description\')]');
+        element.setValue(' I did this and that to fluent QA Test Automation');
+        browser.pause(3000);
+    });
+
+    it('should click Save button', () => {
+        const element = $('//*[@id="root"]/div/div/div[4]/div/div/form/div[3]/div/button');
+        element.click();
+        browser.pause(1000);
+    });
+
+});
+
---
+++ test/specs/Users.spec.js
@@ -0,0 +1,65 @@
+const {expect} = require('chai');
+const {admin, urlLogin} = require('./register_data');
+
+describe('Select student Roles users', () => {
+    it('should Login as admin', () => {
+        browser.url(urlLogin);
+        $('form input[name="email"]').setValue(admin.emailLogin);
+        $('form input[name="password"]').setValue(admin.password);
+        $('form button[type="submit"]').click();
+        browser.pause(2000);
+    });
+
+    it('should go to Users page', () => {
+        const element = $('#site-menu > ul > li:nth-child(7) > a');
+        element.click();
+    });
+
+    it('should have Users h1 title', () => {
+        const actualH1Title = $('#root > div > div > div.container > div > div > h1 > span').getText();
+        const expectedH1Title = 'Users';
+        expect(actualH1Title).eq(expectedH1Title);
+        browser.pause(1000);
+    });
+
+    it('should click Refresh button', () => {
+        const element = $('#root > div > div > div.container > div > div > h1 > button');
+        element.click();
+        browser.pause(3000);
+    });
+
+    // it('should fill up Name text field', () => {
+    //     const element = $('form input[name="name"]');
+    //     element.setValue('Sulaiman');
+    //     browser.pause(2000);
+    // });
+    //
+    // it('should check the Sulaiman user is their', () => {
+    //     const actualName = $('#root > div > div > div.container > div > div > div > div > div > div.rt-table > div.rt-tbody > div > div > div:nth-child(2) > div > div.d-flex.flex-row > a').getText();
+    //     const expectedName=actual.includes('Sulaiman');
+    //     expect(actualName).notInclude(expectedName);
+    // });
+
+    // it('should click button Delete  next to user with Sulaiman name', () => {
+    //     const element = $('#root > div > div > div.container > div > div > div > div > div > div.rt-table > div.rt-tbody > div > div > div:nth-child(5) > button');
+    //     element.click();
+    //     browser.pause(2000);
+    // });
+
+
+    it('should click Roles dropdown list', () => {
+       // const element=$('//body/div[@id=\'root\']/div/div/div[@class=\'container\']/div[@class=\'row\']/div[@class=\'col\']/div/div/form/div[@class=\'row\']/div[2]/div[1]/div[1]/div[1]');
+       const element=$('#root > div > div > div.container > div > div > div > div > form > div:nth-child(2) > div:nth-child(2) > div > div > div > div.css-1hwfws3 > div.css-1wa3eu0-placeholder');
+        element.click();
+        browser.pause(2000);
+        // const el2=$('#root > div > div > div.container > div > div > div > div > form > div:nth-child(2) > div:nth-child(2) > div > div > div > div.css-1hwfws3 > div.css-1g6gooi');
+        // el2.click();
+    });
+
+    // it('should click student option from dropdown list', () => {
+    //     const element=$('//div[contains(@class,\'css-1uccc91-singleValue\')]').getText();
+    //     element.selectByVisibleText('student');
+    //     browser.pause(3000);
+    // });
+
+});
